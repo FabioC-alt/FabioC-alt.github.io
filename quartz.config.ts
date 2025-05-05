5f790244b5725ac4318da569a1f08d3ccc34e263
FabioC-alt <fabiociraci41@gmail.com>
2025-05-05 16:59:50 +0200

    Quartz sync: May 5, 2025, 4:59 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "FabioC-alt.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
